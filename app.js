177d92bd5913e9611d38ee3175cae251c105f9ed
Aleksandra Ujvari <ujvari65@gmail.com>
2022-06-27 20:55:49 +0200

    fix errors in html.

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const bookCollection = [
-    {
-      title: 'The Sulivan',
-      author: 'Mario Puzo',
-    },
-    {
-      title: 'The Poison Arrow Tree',
-      author: 'Shel Arensen',
-    },
-  ];
-  
-  function addBook(book) {
-    bookCollection.push(book);
-  }
-  
-  function removeBook(index) {
-    bookCollection.splice(index, 1);
-  }
-  
-  function displayBooks(books) {
-    const booksContainer = document.querySelector('#book-collection');
-  
-    const booksHtml = books.map(
-      ({ title, author }) => `
+  {
+    title: 'The Sulivan',
+    author: 'Mario Puzo',
+  },
+  {
+    title: 'The Poison Arrow Tree',
+    author: 'Shel Arensen',
+  },
+];
+
+function addBook(book) {
+  bookCollection.push(book);
+}
+
+function removeBook(index) {
+  bookCollection.splice(index, i + 1);
+}
+
+function displayBooks(books) {
+  const booksContainer = document.querySelector('#book-collection');
+
+  const booksHtml = books.map(
+    ({ title, author }) => `
               <div>
                   <h2>${title}</h2>
                   <p>${author}</p>
@@ -29,43 +29,48 @@ const bookCollection = [
                   <hr />
               <div>
           `,
-    );
-  
-    booksContainer.innerHTML = booksHtml.join('');
+  );
+
+
+  booksContainer.innerHTML = booksHtml.join('');
+}
+
+
+
+
+
+
+
+window.onload = () => {
+  const bookForm = document.getElementById('bookForm')
+
+  bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const bookTitle = document.getElementById('title');
+    const bookAuthor = document.getElementById('author');
+
+    let newBook = {
+      title: bookTitle.value,
+      author: bookAuthor.value,
+    };
+
+    addBook(newBook);
+    displayBooks(bookCollection);
   }
-  
-  
-  
-  
-  
-  
-  
-  window.onload = () => {
-     const bookForm = document.getElementById('bookForm')
-  
-      bookForm.addEventListener('submit',  (event) => {
-           event.preventDefault();
-          
-      const bookTitle = document.getElementById('title');
-      const bookAuthor = document.getElementById('author');
-  
-      let newBook = { title: bookTitle.value,
-      author: bookAuthor.value,};
-  
-      addBook(newBook);
-      displayBooks(bookCollection); 
-      }
-      );
-  
-  displayBooks(bookCollection); 
-   }
-  
-  // remove div on click
-  const removeButton = document.getElementById('remove-btn');
-  const bookDiv = document.querySelectorAll('.book');
-  
-  function removeDiv () {
-      bookDiv.style.display = 'none';
-  }
-  
-  removeButton.addEventListener('click', removeDiv);
+  );
+
+  displayBooks(bookCollection);
+}
+
+// remove div on click
+const removeButton = document.getElementById('remove-btn');
+const bookDiv = document.querySelectorAll('.book');
+
+
+
+function removeDiv() {
+  bookDiv.style.display = 'none';
+}
+
+removeButton.addEventListener('click', removeBook);
